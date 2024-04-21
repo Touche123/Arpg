afff5b85c007605a7d34cad93dc83b0432c2455e
Jonas Westman <jonaswestman@hotmail.com>
2024-04-21 11:03:04 +0200

    init files

---
+++ Game1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Arpg
+{
+	public class Game1 : Game
+	{
+		Texture2D playerTexture;
+		
+
+		private GraphicsDeviceManager _graphics;
+		private SpriteBatch _spriteBatch;
+		private SpriteFont _font;
+		private MainMenu _mainMenu;
+
+		public Game1()
+		{
+			_graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+		}
+
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+			
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			_spriteBatch = new SpriteBatch(GraphicsDevice);
+			_font = Content.Load<SpriteFont>("Font");
+			playerTexture = Content.Load<Texture2D>("player");
+
+			string[] menuItems = { "Start Game", "Options", "Exit" };
+			_mainMenu = new MainMenu(menuItems, _font);
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			MouseState mouseState = Mouse.GetState();
+			_mainMenu.Update(mouseState);
+			// TODO: Add your update logic here
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			_spriteBatch.Begin();
+			_spriteBatch.Draw(playerTexture, new Vector2(0, 0), Color.White);
+			_mainMenu.Draw(_spriteBatch);
+			_spriteBatch.End();
+
+			// TODO: Add your drawing code here
+
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ MainMenu.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arpg
+{
+	internal class MenuItem
+	{
+		public string Text { get; set; }
+		private Rectangle _bounds;
+		private Color _color;
+		public bool IsHighlighted { get; set; }
+
+		public MenuItem(string text, Rectangle bounds)
+		{
+			Text = text;
+			_bounds = bounds;
+			_color = Color.White;
+		}
+
+		public void Update(MouseState mouseState)
+		{
+			IsHighlighted = _bounds.Contains(mouseState.Position);
+		}
+
+		public void Draw(SpriteBatch spriteBatch, SpriteFont font)
+		{
+			spriteBatch.DrawString(font, Text, new Vector2(_bounds.X, _bounds.Y), IsHighlighted ? Color.Yellow : _color);
+		}
+	}
+
+	internal class MainMenu
+	{
+		private SpriteFont _font;
+		private readonly MenuItem[] _items;
+		private int _selectedIndex;
+
+		public MainMenu(string[] itemTexts, SpriteFont font)
+		{
+			_font = font;
+			_items = new MenuItem[itemTexts.Length];
+			for (int i = 0; i < itemTexts.Length; i++)
+			{
+				_items[i]  =new MenuItem(itemTexts[i], new Rectangle(100, 100 + i * 40, 200, 30));
+			}
+
+			_selectedIndex = 0;
+		}
+
+		public void Update(MouseState mouseState)
+		{
+			for (int i = 0; i < _items.Length; i++)
+			{
+				_items[i].Update(mouseState);
+				if (_items[i].IsHighlighted && mouseState.LeftButton == ButtonState.Pressed)
+				{
+					_selectedIndex = i;
+					Debug.WriteLine("Selected: " + _items[_selectedIndex].Text);
+					//Console.WriteLine("Selected: " + _items[_selectedIndex].Text);
+				}
+			}
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			for (int i = 0; i < _items.Length; i++)
+			{
+				_items[i].Draw(spriteBatch, _font);
+			}
+		}
+	}
+}
